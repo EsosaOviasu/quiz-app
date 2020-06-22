8bde19f417893121c1c9e76a538996775b07cd06
Esosa Oviasu <koopaoviasu@gmail.com>
2020-06-21 23:42:51 -0400

    added template functions

--- index.js
+++ index.js
@@ -21,45 +21,47 @@ function updateQuizQuestionAndOptions() {
     return i;
 };
 
+function currentQuesAndScore(ques,scor) {
+    return `<h1>Dance Community Quiz</h1>
+                <div id="questionNumAndScore">
+                    <h2 class="questionNum">Question: `+ques+ `/5</h2>
+                    <h2 class="score">Score: `+scor+ `/5</h2>
+                </div>`
+};
 
+function quizBoxContent() {
+    return `<form id="js-questions">
+                <h3 class="js-question"></h3>
+                <div class="row-question">
+                    <input type="radio" id="${questionSet[i].options[0]}" name="pick-one" value="${questionSet[i].options[0]}">
+                        <label for="${questionSet[i].options[0]}" class="optionOne"></label>
+                </div>
+                <div class="row-question">
+                    <input type="radio" id="${questionSet[i].options[1]}" name="pick-one" value="${questionSet[i].options[1]}">
+                        <label for="${questionSet[i].options[1]}" class="optionTwo"></label>
+                </div>
+                <div class="row-question">
+                    <input type="radio" id="${questionSet[i].options[2]}" name="pick-one" value="${questionSet[i].options[2]}">
+                        <label for="${questionSet[i].options[2]}" class="optionThree"></label>
+                </div>
+                <div class="row-question">
+                    <input type="radio" id="${questionSet[i].options[3]}" name="pick-one" value="${questionSet[i].options[3]}">
+                        <label for="${questionSet[i].options[3]}" class="optionFour"></label>
+                </div>
+                <div class="row-question">
+                    <input type="radio" id="${questionSet[i].options[4]}" name="pick-one" value="${questionSet[i].options[4]}">
+                        <label for="${questionSet[i].options[4]}" class="optionFive"></label>
+                </div>
+                <div class="submit-button">
+                    <button type="submit" id="validate">Submit</button>
+                    <button type="button" id="next">Next</button>
+                </div>
+            </form>`
+};
 
 function generateQuestions() {
-    $('.quiz-title').html(`
-        <h1>Dance Community Quiz</h1>
-            <div id="questionNumAndScore">
-                <h2 class="questionNum">Question: ${currentQuestion}/5</h2>
-                <h2 class="score">Score: ${currentScore}/5</h2>
-            </div>
-`)
-    $('.quiz-box').html(`
-        <form id="js-questions">
-            <h3 class="js-question"></h3>
-            <div class="row-question">
-                 <input type="radio" id="${questionSet[i].options[0]}" name="pick-one" value="${questionSet[i].options[0]}">
-                     <label for="${questionSet[i].options[0]}" class="optionOne"></label>
-            </div>
-            <div class="row-question">
-                <input type="radio" id="${questionSet[i].options[1]}" name="pick-one" value="${questionSet[i].options[1]}">
-                     <label for="${questionSet[i].options[1]}" class="optionTwo"></label>
-            </div>
-            <div class="row-question">
-             <input type="radio" id="${questionSet[i].options[2]}" name="pick-one" value="${questionSet[i].options[2]}">
-                <label for="${questionSet[i].options[2]}" class="optionThree"></label>
-            </div>
-            <div class="row-question">
-             <input type="radio" id="${questionSet[i].options[3]}" name="pick-one" value="${questionSet[i].options[3]}">
-                <label for="${questionSet[i].options[3]}" class="optionFour"></label>
-            </div>
-            <div class="row-question">
-             <input type="radio" id="${questionSet[i].options[4]}" name="pick-one" value="${questionSet[i].options[4]}">
-                <label for="${questionSet[i].options[4]}" class="optionFive"></label>
-            </div>
-            <div class="submit-button">
-                <button type="submit" id="validate">Submit</button>
-                <button type="button" id="next">Next</button>
-            </div>
-        </form>
-    `)
+    $('.quiz-title').html(currentQuesAndScore(`${currentQuestion}`,`${currentScore}`));
+    $('.quiz-box').html(quizBoxContent());
     $('#next').hide();
     updateQuizQuestionAndOptions();
     console.log('generateQuestion has run');
@@ -76,11 +78,13 @@ function handleQuizQuestions() {
 };
 
 
-function addValidationMsg(aString) {
-    $("input[name=pick-one]:checked").parent().append(aString)
-    $('input').not(':checked').parent().find('p').remove();
+function validMessage() {
+    return `<p class="correct">Correct!</p>`
 };
 
+function rejectMessage() {
+    return `<p class="incorrect">Sorry, the correct answer is ${questionSet[i-1].answer}</p>`
+};
 
 function validateQuizAnswer() {
     $('.quiz-box').on('submit', '#js-questions', function(event) {
@@ -91,12 +95,12 @@ function validateQuizAnswer() {
             return;
         }
         if ($("input[name=pick-one]:checked").val() === questionSet[i-1].answer) {
-            msg= `<p class="correct">Correct!</p>`
-            addValidationMsg(msg);
+            $("input[name=pick-one]:checked").parent().append(validMessage());
+            $('input').not(':checked').parent().find('p').remove();
         }
         else {
-            msg= `<p class="incorrect">Sorry, the correct answer is ${questionSet[i-1].answer}</p>`
-            addValidationMsg(msg);
+            $("input[name=pick-one]:checked").parent().append(rejectMessage());
+            $('input').not(':checked').parent().find('p').remove();
         }
         $('#next').show();
         $('#validate').hide();
@@ -120,17 +124,23 @@ function updateQuizScore() {
     console.log('updateQuizScore has ran')
 };
 
+
+function quizTitle() {
+    return `<h1>Dance Community Quiz</h1>`
+};
+
+function congratsMessage(currScor) {
+    return `<legend>Congratulations on completing this quiz!</legend>
+                <p>Your final score was ` +currScor+ `/5</p>
+                    <div class="submit-button">
+                        <button type="button" id="restart">Try Again</button>
+                    </div>`
+};
+
+
 function showResults() {
-    $('.quiz-title').html(`
-        <h1>Dance Community Quiz</h1>
-    `)
-    $('.quiz-box').html(`
-        <legend>Congratulations on completing this quiz!</legend>
-            <p>Your final score was ${currentScore}/5</p>
-                <div class="submit-button">
-                    <button type="button" id="restart">Try Again</button>
-                </div>
-    `);
+    $('.quiz-title').html(quizTitle());
+    $('.quiz-box').html(congratsMessage(`${currentScore}`));
     currentQuestion = 1;
     currentScore = 0;
     i = 0;
